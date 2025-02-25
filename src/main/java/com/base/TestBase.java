911a41e38c1430def5b29e1d1aa9cb1a9dd45f94
Piyush Thakur <capvyo@gmail.com>
2025-02-25 13:31:07 +0530

    changes in setup method added optional annotation

--- src/main/java/com/base/TestBase.java
+++ src/main/java/com/base/TestBase.java
@@ -1,6 +1,7 @@
 package com.base;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import com.main.Keywords;
@@ -12,10 +13,12 @@ public class TestBase {
 	/**
 	 * launch the browser before each test case
 	 * @throws Exception
+	 * ( String url, 
+                   String browserName)
 	 */
 	@BeforeMethod
 	@Parameters({"browserName", "url"})
-	public void setUp(String browserName, String url) throws Exception {
+	public void setUp(@Optional("Chrome") String browserName, @Optional("http://localhost/orangehrm") String url) throws Exception {
 		Keywords kw = new Keywords();
 		kw.launchBrowser(browserName);
 		kw.launchURL(url);
