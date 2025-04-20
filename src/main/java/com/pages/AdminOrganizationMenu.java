daa75d8eee9b2dc95263eff68fa0393ee6f59a4f
Piyush Thakur <capvyo@gmail.com>
2025-04-20 11:43:08 +0530

    test case added

---
+++ src/main/java/com/pages/AdminOrganizationMenu.java
@@ -0,0 +1,275 @@
+package com.pages;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.main.Keywords;
+
+public class AdminOrganizationMenu {
+	
+	private Keywords kw;
+	public AdminOrganizationMenu(Keywords kw) {
+		PageFactory.initElements(Keywords.driver, this);
+		this.kw = kw;
+	}
+	
+	private static final Logger LOG = Logger.getLogger(AdminOrganizationMenu.class);
+	
+	@FindBy(css = "nav.oxd-topbar-body-nav li:nth-of-type(3) span.oxd-topbar-body-nav-tab-item")
+	WebElement orgMenu;
+	
+	public void clickOnOrgMenu() {
+		kw.waitForElementToBeVisible(orgMenu);
+		orgMenu.click();
+		LOG.info("Cliked on Organization Menu");
+	}
+	
+	
+	@FindBy(css="#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > div")
+	WebElement pageLoadStatus ;
+	
+	@FindBy(css="div.oxd-table-body > div:nth-child(1) div.oxd-table-cell")
+	List<WebElement> menuList ;
+	
+	public void clickOnOrgSubMenu(String optionText) {
+		kw.waitForElementToBeVisible(orgMenu);
+	    kw.scrollToElement(orgMenu);
+	    kw.waitForElementToBeClickable(orgMenu);
+	    orgMenu.click();
+	    LOG.info("Clicked on the organization Menu in Admin");
+	    // Find all menu options
+	    kw.waitForAllElementAreVisible(menuList);
+	    
+	    List<WebElement> menuOptions = kw.getDriver().findElements(By.cssSelector("ul.oxd-dropdown-menu a.oxd-topbar-body-nav-tab-link"));
+
+	    for (WebElement option : menuOptions) {
+	        if (option.getText().trim().equals(optionText)) {
+	            kw.waitForElementToBeClickable(option);
+	            option.click();
+	            LOG.info("Clicked on : " + optionText);
+	            kw.waitForTextToBe(pageLoadStatus, optionText);
+	            return;
+	            
+	        }
+	    }
+	    LOG.error("Option not found: " + optionText);
+	}
+	
+	@FindBy(css="div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > div > div > label")
+	WebElement editButton ;
+	
+	public void clickOnEditButton() {
+		kw.waitForElementToBeVisibleShort(editButton, 5);
+		editButton.click();
+		LOG.info("Cliked on edit button in organization Menu");
+	}
+	
+	@FindBy(css="form > div:nth-child(2) > div > div:nth-child(1) > div > div:nth-child(2) > input")
+	WebElement registrationNumber ;
+	
+	public void updateRegNum(String s) {
+		kw.visibilityOfElementLocated(registrationNumber);
+		kw.scrollToElement(registrationNumber);
+		registrationNumber.click();
+		registrationNumber.sendKeys(Keys.CONTROL + "A");
+		registrationNumber.sendKeys(Keys.BACK_SPACE);
+		registrationNumber.sendKeys(s);
+		LOG.info("updated the registration number to "+s);
+	}
+	
+	public boolean regNumberCheck(String s) {
+		boolean isEqual = false;
+		kw.visibilityOfElementLocated(registrationNumber);
+		kw.scrollToElement(registrationNumber);
+		String st = (String)kw.JSExecutor("return arguments[0].value;", registrationNumber);
+		isEqual = st.equals(s);
+		if(isEqual == true) {
+			LOG.info("registration number matched");
+			return isEqual;
+		}else {
+			LOG.info("registration number does not match "+st);
+			return isEqual;
+		}
+		
+		
+	}
+	
+	@FindBy(css="div.organization-name-container input")
+	WebElement  orgName;
+	
+	public void updateOrgName(String s) {
+		kw.visibilityOfElementLocated(orgName);
+		kw.scrollToElement(orgName);
+		orgName.click();
+		orgName.sendKeys(Keys.CONTROL + "A");
+		orgName.sendKeys(Keys.BACK_SPACE);
+		orgName.sendKeys(s);
+		LOG.info("Updated organization name to "+s);
+	}
+	
+	public boolean orgNameTextCheck(String s) {
+		boolean isEqual = false;
+		kw.visibilityOfElementLocated(orgName);
+		kw.scrollToElement(orgName);
+		String st = (String)kw.JSExecutor("return arguments[0].value;", orgName);
+		isEqual = st.equals(s);
+		if(isEqual == true) {
+			LOG.info("Organization name matched");
+			return isEqual;
+		}else {
+			LOG.info("Organization name does not match"+st);
+			return isEqual;
+		}
+		
+	}
+	
+	@FindBy(css="form > div:nth-child(2) > div > div:nth-child(2) > div > div:nth-child(2) > input")
+	WebElement taxID ;
+	
+	public void updateTaxID(String s) {
+		kw.visibilityOfElementLocated(taxID);
+		kw.scrollToElement(taxID);
+		taxID.click();
+		taxID.sendKeys(Keys.CONTROL + "A");
+		taxID.sendKeys(Keys.BACK_SPACE);
+		taxID.sendKeys(s); //PVT1419S8809
+		LOG.info("Updated the Tax ID to "+s);
+	}
+	
+	public boolean taxIDtextCheck(String s) {
+		boolean isEqual = false;
+		kw.visibilityOfElementLocated(taxID);
+		kw.scrollToElement(taxID);
+		String st = (String)kw.JSExecutor("return arguments[0].value;", taxID);
+		isEqual = st.equals(s);
+		if(isEqual == true) {
+			LOG.info("TaxID number matched");
+			return isEqual;
+		}else {
+			LOG.info("TaxID number does not match "+st);
+			return isEqual;
+		}
+	}
+
+	
+	@FindBy(css="form > div.oxd-form-actions > button")
+	WebElement saveButton ;
+	
+	public void clickOnSave() {
+		kw.waitForElementToBeVisible(saveButton);
+		kw.scrollToElement(saveButton);
+		saveButton.click();
+	}
+	
+	@FindBy(css=".oxd-toast.oxd-toast--success.oxd-toast-container--toast")
+	WebElement saveSuccessfullToast ;
+	
+	/**
+	 * @return save successful toast message
+	 * @throws TimeoutException 
+	 */
+	public boolean SaveToastMessageText() throws TimeoutException {
+		try {
+			kw.waitForElementToBeVisibleShort(saveSuccessfullToast,5);
+			kw.scrollToElement(saveSuccessfullToast);
+			boolean isDisplayed = saveSuccessfullToast.isDisplayed();
+			LOG.info("Successfully added orgnization details");
+			return isDisplayed;
+		} catch (TimeoutException e) {
+			LOG.warn("Toast message not found within timeout.");
+	        
+		}catch (NoSuchElementException e) {
+	        LOG.warn("Toast element not found in DOM.");
+	    }
+		return false;
+		
+		
+	}
+	
+	@FindBy(css="form > div.oxd-form-row > div > div:nth-child(1) > div > div:nth-child(2) > input")
+	WebElement locationNameSearchText;
+	
+	@FindBy(css="div.oxd-form-actions > button.oxd-button.oxd-button--medium.oxd-button--secondary.orangehrm-left-space")
+	WebElement searchButton;
+	
+	public void searchLocation(String nm) {
+		//kw.waitForElementToBeClickable(locationNameSearchText);
+		//kw.scrollToElement(locationNameSearchText);
+		locationNameSearchText.click();
+		locationNameSearchText.sendKeys(nm);
+		locationNameSearchText.sendKeys(Keys.TAB);
+		searchButton.click();
+		LOG.info("Successfully searched the location with name " + nm);
+	}
+	
+	@FindBy(css=".oxd-toast.oxd-toast--error.oxd-toast-container--toast")
+	WebElement errorToast ;
+	
+	/**
+	 * Check if the element is present or not
+	 * @return true if the error toast is displayed
+	 * @throws TimeoutException
+	 */
+	public boolean errorToastMessageText() throws TimeoutException {
+		try {
+			kw.waitForElementToBeVisibleShort(errorToast,5);
+			kw.scrollToElement(errorToast);
+			boolean isDisplayed = errorToast.isDisplayed();
+			LOG.info("Error toast found ");
+			return isDisplayed;
+		} catch (TimeoutException e) {
+			LOG.warn("Toast message not found within timeout.");
+	        
+		}catch (NoSuchElementException e) {
+	        LOG.warn("Toast element not found in DOM.");
+	    }
+		return false;
+	}
+
+	
+	@FindBy(css="i.oxd-icon.bi-trash.oxd-button-icon")
+	WebElement confirmDeleteYes ;
+	
+	@FindBy(css="div.oxd-table-row")
+	List<WebElement> searchResultList;
+	
+	public void deleteSelected(String loc) {
+
+		kw.waitForAllElementAreVisible(searchResultList);
+		boolean isLocationFound = false;
+		WebElement found = null;
+		for(WebElement row: searchResultList) {
+			if(row.getText().contains(loc)) {
+				isLocationFound = true;
+				found = row;
+				break;
+			}else {
+				isLocationFound = false;
+			}
+		}
+			if(isLocationFound) {
+				LOG.info("Found the reporting Method");
+				WebElement deleteButton = found.findElement(By.cssSelector("button.oxd-icon-button > i.oxd-icon.bi-trash"));
+				deleteButton.click();
+				if(!errorToastMessageText()) {
+					kw.waitForElementToBeClickable(confirmDeleteYes);
+			        confirmDeleteYes.click();
+			        LOG.info("Successfully Deleted the location "+loc);
+				}else {
+					LOG.error("The location already in use");
+				}
+			}else {
+				LOG.info("No such location found");
+			}
+			kw.waitInvisibilityOfElement(confirmDeleteYes);
+	}
+	
+}
--- src/test/java/com/testcases/TestSprint2.java
+++ src/test/java/com/testcases/TestSprint2.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 import com.base.TestBase;
 import com.pages.AdminJobTitles;
 import com.pages.AdminMenu;
+import com.pages.AdminOrganizationMenu;
 import com.pages.LoginPage;
 
 import io.qameta.allure.Description;
@@ -19,15 +20,17 @@ import io.qameta.allure.Story;
 
 public class TestSprint2 extends TestBase {
 	
-	public LoginPage login;
-	public AdminMenu admin;
-	public AdminJobTitles jobs;
+	private LoginPage login;
+	private AdminMenu admin;
+	private AdminJobTitles jobs;
+	private AdminOrganizationMenu orgMenu;
 
 	@BeforeMethod
 	public void pageSetup() {
         login = new LoginPage(kw); 
         admin = new AdminMenu(kw);
         jobs = new AdminJobTitles(kw);
+        orgMenu = new AdminOrganizationMenu(kw);
         
     }
 	
@@ -79,6 +82,7 @@ public class TestSprint2 extends TestBase {
 	public void checkForJobMenuAreShowingCorrectOptions() throws InterruptedException {
 		
 		login.logMeIn();
+		admin.clickOnAdminMenu();
 		
 		
 		jobs.clickOnAdminMenu();
@@ -89,20 +93,48 @@ public class TestSprint2 extends TestBase {
 		jobs.clickOnJobSubMenu("Employment Status");
 		Assert.assertTrue(jobs.checkElementinTable("Active"));
 		jobs.clickOnJobSubMenu("Job Categories");
-		Assert.assertTrue(jobs.checkElementinTable("Craft Workers"));
-		jobs.clickOnJobSubMenu("Work Shifts");
-		Assert.assertTrue(jobs.checkElementinTable("Day Shift"));
 		
+	
 		
 	}
 	
 	
-	@Test(enabled = false)
+
+	@Test
+	@Severity(SeverityLevel.NORMAL)
+    @Description("Checking admin page options organization submenu")
+    @Step("login and navigate to admin page, check organization options")
+    @Feature("admin Features Organization menu")
+    @Story("Admin page options oragnization submenu checking")
+	public void checkForOrganizationMenuAreShowingCorrectOptions() throws InterruptedException {
+		
+		login.logMeIn();
+		admin.clickOnAdminMenu();
+		
+		orgMenu.clickOnOrgMenu();
+		orgMenu.clickOnOrgSubMenu("General Information");
+		orgMenu.clickOnEditButton();
+		orgMenu.updateRegNum("123456");
+		orgMenu.updateOrgName("PsychoCorps");
+		orgMenu.updateTaxID("PVT1419S8809");
+		orgMenu.clickOnSave();
+		Assert.assertTrue(orgMenu.SaveToastMessageText());
+		Assert.assertTrue(orgMenu.orgNameTextCheck("PsychoCorps"));
+		Assert.assertTrue(orgMenu.taxIDtextCheck("PVT1419S8809"));
+		Assert.assertTrue(orgMenu.regNumberCheck("123456"));
+	}
+	
+	
+	@Test(enabled = true)
 	public void TryAndError() throws InterruptedException {
 		
 		login.logMeIn();
-		
-		//AdminJobTitles jobs = new AdminJobTitles();
+		admin.clickOnAdminMenu();
+		orgMenu.clickOnOrgMenu();
+		orgMenu.clickOnOrgSubMenu("Locations");
+		orgMenu.searchLocation("MumbaiCBO");
+		//orgMenu.deleteSearchedLocation("MumbaiCBO");
+		orgMenu.deleteSelected("MumbaiCBO");
 		
 		
 	}
